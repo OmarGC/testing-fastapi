0484a96fcad2320c590916aae91f7e31f34adc4d
profesor <omar.psytrance9@gmail.com>
2022-01-07 18:48:43 -0600

    test 2

--- app/models/user.py
+++ app/models/user.py
@@ -13,6 +13,7 @@ user = Table(
     Column( "created_at", DateTime(timezone=True), server_default=func.now(), nullable=False ),
     Column( "updated_at", DateTime(timezone=True), onupdate=func.now(), nullable=True ),
     Column( "nombre_usuario", String(120), nullable=False ),
+    Column( "password_usuario", String(300), nullable=False ),
     Column( "apellido_paterno_usuario", String(120), nullable=True ),
     Column( "apellido_materno_usuario", String(120), nullable=True ),
     Column( "email_usuario", String(150), nullable=False, unique=True ),
